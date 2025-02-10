689bd9af39bf48c76a42b04dabf6fb09d82c4781
MaelleCuenot <maelle.cuenot@edu.ece.fr>
2025-02-10 15:21:59 +0100

    Ajout des modifications pour la branche maelle

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { Chat } from '~/components/chat/Chat.client';
 import { Header } from '~/components/header/Header';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Genia' }, { name: 'description', content: 'Travail en collaboration avec Genia, modèle améliorer pour site web de Bolt' }];
 };
 
 export const loader = () => json({});
